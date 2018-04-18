c18ed475d7c98f3a12c05893b5e5e78cd1d25380
Sean Delaney <hello@delaneymethod.com>
2018-04-18 16:07:47 +0100

    Added base Employees React component.

--- src/assets/js/components/App.jsx
+++ src/assets/js/components/App.jsx
@@ -11,12 +11,14 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 import Shifts from './shifts';
+import Employees from './employees';
 
 const App = () => (
 	<Switch>
 		<Route exact path='/' component={Home} />
 		<Route exact path='/login' component={Login} />
 		<Route exact path='/shifts' component={Shifts} />
+		<Route exact path='/employees' component={Employees} />
 	</Switch>
 );
 
---
+++ src/assets/js/components/employees/Index.jsx
@@ -0,0 +1,15 @@
+/**
+ * @link https://www.giggrafter.com
+ * @copyright Copyright (c) Gig Grafter
+ * @license https://www.giggrafter.com/license
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { hot } from 'react-hot-loader';
+
+const Employees = () => <h1>Employees page</h1>;
+
+Employees.propTypes = {};
+
+export default hot(module)(Employees);
