b5ecea1d0d89ad89d2d62746a9eb7f745ed54778
Sean Delaney <hello@delaneymethod.com>
2018-04-18 15:51:27 +0100

    Added base Shifts React component.
    - Also correctly formatted the license in the main SCSS file.
    - Removed duplicate package reference.

--- src/assets/js/components/App.jsx
+++ src/assets/js/components/App.jsx
@@ -10,11 +10,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './Home';
 import Login from './Login';
+import Shifts from './shifts';
 
 const App = () => (
 	<Switch>
 		<Route exact path='/' component={Home} />
 		<Route exact path='/login' component={Login} />
+		<Route exact path='/shifts' component={Shifts} />
 	</Switch>
 );
 
---
+++ src/assets/js/components/shifts/Index.jsx
@@ -0,0 +1,15 @@
+/**
+ * @link https://www.giggrafter.com
+ * @copyright Copyright (c) Gig Grafter
+ * @license https://www.giggrafter.com/license
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { hot } from 'react-hot-loader';
+
+const Shifts = () => <h1>Shifts page</h1>;
+
+Shifts.propTypes = {};
+
+export default hot(module)(Shifts);
