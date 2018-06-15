0f5e7f5a29228207c81964440c51a6c845eb517a
Barry Lynch <barry.lynch@giggrafter.com>
2018-06-15 09:29:17 +0100

    moving publish into toolbar

--- src/assets/js/components/common/Toolbar.jsx
+++ src/assets/js/components/common/Toolbar.jsx
@@ -15,7 +15,7 @@ import ShiftForm from '../forms/ShiftForm';
 
 import { switchWeek } from '../../actions/weekActions';
 
-import { getRotas, switchRota } from '../../actions/rotaActions';
+import { getRotas, switchRota, updateRota } from '../../actions/rotaActions';
 
 import { getRotaTypes, switchRotaType } from '../../actions/rotaTypeActions';
 
@@ -46,6 +46,8 @@ class Toolbar extends Component {
 		this.handleCreateShift = this.handleCreateShift.bind(this);
 
 		this.handleSwitchRotaType = this.handleSwitchRotaType.bind(this);
+
+		this.handlePublishRota = this.handlePublishRota.bind(this);
 	}
 
 	getInitialState = () => ({
@@ -127,6 +129,17 @@ class Toolbar extends Component {
 		}
 	};
 
+	handlePublishRota = () => {
+		const { actions } = this.props;
+		const { rota } = this.props;
+
+		actions.updateRota(rota).catch((error) => {
+			this.setState({ error });
+
+			this.handleModal();
+		});
+	};
+
 	handleModal = () => this.setState({ isErrorModalOpen: !this.state.isErrorModalOpen });
 
 	handleCreateRota = () => this.setState({ isRotaModalOpen: !this.state.isRotaModalOpen });
@@ -152,6 +165,7 @@ class Toolbar extends Component {
 				</Col>
 				<Col className="pt-3 pb-3 pt-sm-3 pb-ms-3 text-center text-sm-right" xs="12" sm="6" md="6" lg="6" xl="6">
 					<button type="button" title="Add New Shift" className="btn btn-secondary pl-5 pr-5 pl-md-4 pr-md-4 pl-lg-5 pr-lg-5 border-0" onClick={this.handleCreateShift}><i className="pr-2 fa fa-plus" aria-hidden="true"></i>Add New Shift</button>
+					<button type="button" title="Publish Rota" className="btn btn-nav btn-primary pl-5 pr-5 pl-md-4 pr-md-4 pl-lg-5 pr-lg-5 border-0" onClick={this.handlePublishRota}>Publish</button>
 					<Modal title="Shifts" className="modal-dialog" buttonLabel="Cancel" show={this.state.isShiftModalOpen} onClose={this.handleCreateShift}>
 						<ShiftForm />
 					</Modal>
@@ -184,6 +198,7 @@ const mapDispatchToProps = dispatch => ({
 		switchRota,
 		getRotaTypes,
 		switchRotaType,
+		updateRota,
 	}, dispatch),
 });
 
