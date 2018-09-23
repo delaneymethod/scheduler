4fffa887136cb9aa6a6a3c12696317a595d711a6
Sean Delaney <hello@delaneymethod.com>
2018-09-23 18:49:47 +0100

    v1.0.10-beta - Always show Login/Register buttons

--- src/assets/js/components/common/SiteNavBar.jsx
+++ src/assets/js/components/common/SiteNavBar.jsx
@@ -32,12 +32,9 @@ class SiteNavBar extends Component {
 							<li className="nav-item p-0 m-0"><a href="#about" title="About" id="about" className="nav-link pt-0 pb-0 pl-3 pr-3 m-0 ml-md-2 btn btn-dark border-0">About</a></li>
 							<li className="nav-item p-0 m-0"><a href="#beta" title="Beta" id="beta" className="nav-link pt-0 pb-0 pl-3 pr-3 m-0 ml-md-2 btn btn-dark border-0">Beta</a></li>
 							<li className="nav-item p-0 m-0"><a href="#pricing" title="Pricing" id="pricing" className="nav-link pt-0 pb-0 pl-3 pr-3 m-0 ml-md-2 btn btn-dark border-0">Pricing</a></li>
-							<li className="d-block d-sm-none nav-item p-0 m-0 spacer">&nbsp;</li>
 							<li className="nav-item p-0 m-0"><a href={routes.NEWS.URI} id="news" title={routes.NEWS.TITLE} className="nav-link pt-0 pb-0 pl-3 pr-3 m-0 ml-md-2 btn btn-dark border-0">{routes.NEWS.TITLE}</a></li>
-							<li className="nav-item p-0 m-0"><a href={routes.LOGIN.URI} id="login" title={routes.LOGIN.TITLE} className="nav-link pt-0 pb-0 pl-3 pr-3 m-0 ml-md-2 btn btn-secondary border-0">{routes.LOGIN.TITLE}</a></li>
-							<li className="nav-item p-0 m-0"><a href={routes.REGISTER.URI} id="register" title={routes.REGISTER.TITLE} className="nav-link pt-0 pb-0 pl-3 pr-3 m-0 ml-md-2 btn btn-primary border-0">{routes.REGISTER.TITLE}</a></li>
-							<li className="d-block d-sm-none nav-item p-0 m-0 spacer">&nbsp;</li>
-							<li className="d-block d-sm-none nav-item p-0 m-0 spacer">&nbsp;</li>
+							<li className="d-none d-lg-block nav-item p-0 m-0"><a href={routes.LOGIN.URI} id="login" title={routes.LOGIN.TITLE} className="nav-link pt-0 pb-0 pl-3 pr-3 m-0 ml-md-2 btn btn-secondary border-0">{routes.LOGIN.TITLE}</a></li>
+							<li className="d-none d-lg-block nav-item p-0 m-0"><a href={routes.REGISTER.URI} id="register" title={routes.REGISTER.TITLE} className="nav-link pt-0 pb-0 pl-3 pr-3 m-0 ml-md-2 btn btn-primary border-0">{routes.REGISTER.TITLE}</a></li>
 						</ul>
 					</div>
 					<div className="p-0 pt-3 p-md-0 m-0 text-center order-3 order-sm-3 order-md-2 order-lg-1 order-xl-1 col-12 col-md-auto">
@@ -47,6 +44,10 @@ class SiteNavBar extends Component {
 						<button type="button" className="text-center btn-lines mt-0 pt-2 pl-2 pr-2 pb-2 border-1 rounded-1 text-white lines-button x navbar-toggler" role="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation" onClick={event => this.handleClick(event)}>
 							<span className="align-middle lines"></span>&nbsp;<span className="">Menu</span>
 						</button>
+						<div className="d-inline-block d-lg-none">
+							<a href={routes.LOGIN.URI} id="login" title={routes.LOGIN.TITLE} className="mt-0 ml-2 pt-0 pl-3 pr-3 pb-0 btn btn-secondary btn-med border-0 med-buttons">{routes.LOGIN.TITLE}</a>
+							<a href={routes.REGISTER.URI} id="register" title={routes.REGISTER.TITLE} className="mt-0 ml-2 pt-0 pl-3 pr-3 pb-0 btn btn-primary btn-med border-0">{routes.REGISTER.TITLE}</a>
+						</div>
 					</div>
 				</nav>
 			</Col>
