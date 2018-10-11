8ca1314844bbdaa5a8404f5eeffd3045ddf6a5c5
Sean Delaney <hello@delaneymethod.com>
2018-10-11 09:27:46 +0100

    New Action

---
+++ src/assets/js/actions/rotaTypeEmployeeActions.js
@@ -0,0 +1,57 @@
+import * as api from '../api';
+import * as types from './actionTypes';
+
+export const ajaxLoading = status => ({
+	type: types.AJAX_LOADING,
+	status,
+});
+
+/* CREATE ROTA TYPE EMPLOYEES */
+export const createRotaTypeEmployeesSuccess = employees => ({
+	type: types.CREATE_ROTA_TYPE_EMPLOYEES,
+	employees,
+});
+
+export const createRotaTypeEmployees = payload => (dispatch) => {
+	dispatch(ajaxLoading(true));
+
+	return api.createRotaTypeEmployees(payload)
+		.then((employees) => {
+			dispatch(ajaxLoading(false));
+
+			dispatch(createRotaTypeEmployeesSuccess(employees));
+
+			return employees;
+		})
+		.catch((error) => {
+			dispatch(ajaxLoading(false));
+
+			/* Bubble the error back up the rabbit hole */
+			return Promise.reject(error);
+		});
+};
+
+/* DELETE SPECIFIC ROTA TYPE EMPLOYEE */
+export const deleteRotaTypeEmployeeSuccess = employee => ({
+	type: types.DELETE_ROTA_TYPE_EMPLOYEE,
+	employee,
+});
+
+export const deleteRotaTypeEmployee = payload => (dispatch) => {
+	dispatch(ajaxLoading(true));
+
+	return api.deleteRotaTypeEmployee(payload)
+		.then((employee) => {
+			dispatch(ajaxLoading(false));
+
+			dispatch(deleteRotaTypeEmployeeSuccess(employee));
+
+			return employee;
+		})
+		.catch((error) => {
+			dispatch(ajaxLoading(false));
+
+			/* Bubble the error back up the rabbit hole */
+			return Promise.reject(error);
+		});
+};
